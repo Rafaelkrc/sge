d34d4f7f7ff06baf051576b074211d6376181097
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-16 17:55:18 +0100

    add sales metrics on dashboard
    close #45

--- app/metrics.py
+++ app/metrics.py
@@ -1,5 +1,7 @@
+from django.db.models import Sum
 from django.utils.formats import number_format
 from products.models import Product
+from outflows.models import Outflows
 
 
 def get_product_metrics():
@@ -13,4 +15,18 @@ def get_product_metrics():
         total_selling_price=number_format(total_selling_price, decimal_pos=2, force_grouping=True),
         total_quantity=total_quantity,
         total_profit=number_format(total_profit, decimal_pos=2, force_grouping=True),
+    )
+
+
+def get_sales_metrics():
+    total_sales = Outflows.objects.count()
+    total_products_sold = Outflows.objects.aggregate(total_products_sold=Sum('quantity'))['total_products_sold'] or 0
+    total_sales_value = sum(outflow.quantity * outflow.product.selling_price for outflow in Outflows.objects.all())
+    total_sales_cost = sum(outflow.quantity * outflow.product.cost_price for outflow in Outflows.objects.all())
+    total_sales_profit = total_sales_value - total_sales_cost
+    return dict(
+        total_sales=total_sales,
+        total_products_sold=total_products_sold,
+        total_sales_value=number_format(total_sales_value, decimal_pos=2, force_grouping=True),
+        total_sales_profit=number_format(total_sales_profit, decimal_pos=2, force_grouping=True),
     )
