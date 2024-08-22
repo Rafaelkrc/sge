c0dd271ec7b3e49785244125939f68c96af9d2e8
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-22 22:25:11 +0100

    criado login e logout de usuário e suas rotas, configurado em todas as opções se o usuário está autenticado
    close #53

--- app/settings.py
+++ app/settings.py
@@ -54,6 +54,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
