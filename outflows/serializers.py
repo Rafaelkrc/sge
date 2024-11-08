83a3b284497fe6d57c6d53aea299cb978990b316
Rafaelkrc <rafaelkrc@hotmail.com>
2024-11-08 15:18:39 +0000

    create urls, seriaizers and views to API
    close #60

---
+++ outflows/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from outflows.models import Outflows
+
+
+class OutflowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Outflows
+        fields = '__all__'
